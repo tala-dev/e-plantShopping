eeafd37f2796106c86e76e827f2c4c5985591ab2
tala-dev <tlatypova.dev@gmail.com>
2026-01-09 12:13:40 -0500

    Task 2

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
+
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      const { payload: plant } = action;
+      console.log("Add item action", action);
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        items.push({ name, image, cost, quantity: 1});
+      }
     },
     removeItem: (state, action) => {
+      console.log("Remove action", action);
+      state.items = state.items.find(item => item.name === action.payload);
     },
     updateQuantity: (state, action) => {
-
+      const { name, quantity } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity = quantity;
+      }
     
     },
   },
